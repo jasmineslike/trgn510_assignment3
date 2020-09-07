03afcba7d416d8d2892bb8a0a1d4ce2a7f19c9ce
jasmineslike <lilixu@trgn.usc.edu>
2020-09-06 20:04:11 -0700

    First commit

---
+++ circumference.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def circumference():
+	pi = 3.14159
+	r = 3
+	valueCir = 2 * pi * r
+
+	print("The circumference of a circle of radius 3 is " + str(valueCir))
+
+circumference()
+
